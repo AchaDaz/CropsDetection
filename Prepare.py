e0f18fa60ebcb18a27a426b25ee52bb60b494c04
an-eliseev <eliseev.a@team.karpov.courses>
2024-01-05 14:25:38 +0300

    start of file

---
+++ Prepare.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+from torch.utils.data import DataLoader
+from torchvision import transforms, datasets
+import matplotlib.pyplot as plt
+from PIL import Image
+
