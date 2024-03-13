e895ad7f5914627efecc8cbafa249f24df7d750b
an-eliseev <eliseev.a@team.karpov.courses>
2024-03-13 17:41:52 +0300

    f

--- main.py
+++ main.py
@@ -5,12 +5,6 @@ from train import train
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-n', '--nodes', default=1, type=int, metavar='N',
-                        help='number of data loading workers (default: 4)')
-    parser.add_argument('-g', '--gpus', default=1, type=int,
-                        help='number of gpus per node')
-    parser.add_argument('-nr', '--nr', default=0, type=int,
-                        help='ranking within the nodes')
     parser.add_argument('-epochs', default=1, type=int, metavar='N',
                         help='number of total epochs to run')
     parser.add_argument('-tl', default=1, type=int, metavar='N',
