dfcc018165ed47ade2f86aee4f24bdd73fb66911
an-eliseev <eliseev.a@team.karpov.courses>
2024-03-13 18:07:30 +0300

    f

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from train import train
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-epochs', default=1, type=int, metavar='N',
+    parser.add_argument('--epochs', default=1, type=int, metavar='N',
                         help='number of total epochs to run')
-    parser.add_argument('-tl', default=1, type=int, metavar='N',
+    parser.add_argument('--tl', default=1, type=int, metavar='N',
                         help='flag pre training/transfer learning')
-    parser.add_argument('-local_rank', type=int, metavar='N',
-                        help='flag master node for saving result')
+    parser.add_argument("--local_rank", type=int, 
+                        help="Local rank. Necessary for using the torch.distributed.launch utility.")
     args = parser.parse_args()
     
     setup()
